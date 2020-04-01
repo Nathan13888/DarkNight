11e71df08d9d7b6bc790edcda5cd8ae3780a4cab
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-04-01 17:02:14 -0400

    Modified and added new colours to the theme Added bold italic variant Some minor bug fixes

--- src/build.js
+++ src/build.js
@@ -4,11 +4,11 @@ const mustache = require('mustache');
 const buildDir = `${__dirname}/../themes`;
 const variables = require('./variables.json');
 const template = JSON.stringify(require(`${__dirname}/template.json`));
-const themeNames = ['darknight', 'darknight-italic', 'darknight-bold'];
+const themeNames = ['darknight', 'darknight-italic', 'darknight-bold', 'darknight-bold-italic'];
 
 let content, theme;
 for (themeName of themeNames) {
-  content = require(`${__dirname}/variants/${themeName}.json`);
-  theme = mustache.render(template, { ...content, ...variables });
-  fs.writeFileSync(`${buildDir}/${themeName}.json`, theme);
+    content = require(`${__dirname}/variants/${themeName}.json`);
+    theme = mustache.render(template, { ...content, ...variables });
+    fs.writeFileSync(`${buildDir}/${themeName}.json`, theme);
 }
