69f5210c7034ab0acffe4774b62ff754012d2b02
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-04-01 13:42:40 -0400

    Changed GitHub links to the new one. Updated dev. dep. minimum versions Reorganized build file structure

--- src/build.js
+++ src/build.js
@@ -1,12 +1,14 @@
 const fs = require('fs');
 const mustache = require('mustache');
 
-const globals = require('./globals.json');
+const buildDir = `${__dirname}/../themes`;
+const variables = require('./variables.json');
 const template = JSON.stringify(require(`${__dirname}/template.json`));
 const themeNames = ['darknight', 'darknight-italic', 'darknight-bold'];
 
+let content, theme;
 for (themeName of themeNames) {
-  const content = require(`${__dirname}/${themeName}.json`);
-  const theme = mustache.render(template, { ...content, ...globals });
-  fs.writeFileSync(`${__dirname}/../themes/${themeName}.json`, theme);
+  content = require(`${__dirname}/variants/${themeName}.json`);
+  theme = mustache.render(template, { ...content, ...variables });
+  fs.writeFileSync(`${buildDir}/${themeName}.json`, theme);
 }
