98cf2fc40f510e0542066fc0265ba24f4a75c3b1
Nathan <nathan13888@gmail.com>
2020-04-01 23:24:19 -0400

    update java example

--- demos/java.java
+++ demos/java.java
@@ -1,37 +1,33 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.Exception;
 
-public class Dog 
-{
-    String name; 
-    String breed; 
-    int age; 
-    String color; 
-  
-    public Dog(String name, String breed, int age, String color) 
-    { 
-        this.name = name; 
-        this.breed = breed; 
-        this.age = age; 
-        this.color = color; 
-    } 
-  
+public class java {
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    public java(String name, String breed, int age, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
     @Override
-    public String toString() 
-    { 
-        return("Hi my name is "+ this.getName()+ 
-               ".\nMy breed,age and color are " + 
-               this.getBreed()+"," + this.getAge()+ 
-               ","+ this.getColor()); 
-    } 
-  
-    public static void main(String[] args) throws Exception
-    {
-        // TODO: Buffered Reader
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            Dog tuffy = new Dog("tuffy","papillon", 5, "white"); 
-            System.out.println(tuffy.toString()); 
+    public String toString() {
+        return ("Hi my name is " + this.getName() + ".\nMy breed,age and color are " + this.getBreed() + ","
+                + this.getAge() + "," + this.getColor());
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String name = br.readLine().trim();
+            String breed = br.readLine().trim();
+            int age = Integer.parseInt(br.readLine().trim());
+            String colour = br.readLine().trim();
+            java newDog = new java(name, breed, age, colour);
+            System.out.println(newDog.toString()); 
         }
     } 
     
