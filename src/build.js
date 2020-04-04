5a1612d7b10861b322fa3ce26cf930e0a91647e7
Nathan <nathan13888@gmail.com>
2020-04-04 18:17:47 -0400

    extras

--- src/build.js
+++ src/build.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const mustache = require('mustache');
+const tache = require('mustache');
 
 const buildDir = `${__dirname}/../themes`;
 const variables = require('./variables.json');
@@ -13,8 +13,23 @@ const themes = [
 ];
 
 let variant, compiled;
+
 for (theme of themes) {
 	variant = require(`${__dirname}/variants/${theme}.json`);
-	compiled = mustache.render(template, { ...variant, ...variables });
+	compiled = tache.render(template, { ...variant, ...variables });
 	fs.writeFileSync(`${buildDir}/${theme}.json`, compiled);
 }
+
+// const extra = require('./extras.json');
+// for (theme of themes) {
+// 	path = `${__dirname}/variants/${theme}`;
+// 	variant = require(path + '.json');
+// 	variant.name = `${variant.name} Extra`;
+// 	fs.writeFile(path, JSON.stringify(variant), function writeJSON(err) {
+// 		if (err) return console.log(err);
+// 		console.log(JSON.stringify(variant));
+// 		console.log('Writing to ' + path);
+// 	});
+// 	compiled = tache.render(template, { ...variant, ...variables, ...tache });
+// 	fs.writeFileSync(`${buildDir}/${theme}-extra.json`, compiled);
+// }
